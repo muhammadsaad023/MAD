db564f2b0761e0b633d5da8ce854385f47960de4
Muhammad Saad <145099224+muhammadsaad023@users.noreply.github.com>
2023-10-23 00:00:23 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,129 +1,66 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView, Linking } from 'react-native';
+import { View, ImageBackground, TextInput, Button, Text, StyleSheet } from 'react-native';
 
-const App = () => {
+const LoginPage = () => {
   return (
-    <ScrollView style={styles.container}>
-      <Header />
-      <Contact /><Summary />
-      <Education />
-      <TopSkills />
-      <Certifications />
-      
-      <Experience />
-    </ScrollView>
-  );
-};
-
-const Header = () => {
-  return (
-    <View style={styles.headerContainer}>
-      <Text style={styles.name}>Muhammad Saad</Text>
-      <Text>Requirement Engineer || Mobile Application developer</Text>
-      <Text>Prompts Engineer || Member of Google Developers Core Team</Text>
-      <Text>Punjab, Pakistan</Text>
-    </View>
-  );
-};
-
-const Contact = () => {
-  return (
-    <View style={styles.contactContainer}>
-      <Text>House # B-85, Gulistan Colony, WahCantt</Text>
-      <Text>Mobile: 03225425081</Text>
-      <Text>Email: saadbhai8760@gmail.com</Text>
-      <Text onPress={() => Linking.openURL('https://github.com/muhammadsaad023/MAD')}>GitHub:github.com/muhammadsaad023</Text>
-      
-    </View>
-  );
-};
-
-const TopSkills = () => {
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>Top Skills</Text>
-      <Text>Prompt Engineering</Text>
-      <Text>Microsoft Excel</Text>
-      <Text>Methodology</Text>
-    </View>
-  );
-};
-
-const Certifications = () => {
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>Certifications</Text>
-      <Text>Swift (Programming language)</Text>
-      <Text>React +Redux</Text>
-    </View>
-  );
-};
-
-const Summary = () => {
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>Summary</Text>
-      <Text>Muhammd Saad | Software Quality Engineer | Comsats Institute of Information and Technology, Attock City, Pakistan</Text>
-      {/*... More summary details */}
-    </View>
-  );
-};
-
-const Experience = () => {
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>Experience</Text>
-      <Text>Indaraj Technologies Pvt. Ltd.</Text>
-      <Text>Software Quality Assurance Engineer, Islamabad, Pakistan</Text>
-      <Text>November 2022 - March 2023 (5 months)</Text>
-    </View>
-  );
-};
-
-const Education = () => {
-  return (
-    <View
-      style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>Education</Text>
-      <Text>Comsats Institute of Information and Technology, Attock City, Pakistan</Text>
-      <Text>BS Software Engineering</Text>
-      <Text>2021 - 2025</Text>
-    </View>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('./assets/backk.jpg')}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.overlay}>
+          <View style={styles.loginContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry={true}
+            />
+            <Button title="Login" onPress={() => {/* Handle login logic */}} />
+            <Text style={styles.forgotPasswordText}>Forgotten Password</Text>
+            <Text style={styles.createAccountText}>Create your account</Text>
+          </View>
+        </View>
+      </ImageBackground>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 10,
-    backgroundColor: 'lightblue'
   },
-  headerContainer: {
-    padding: 15,
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: 'red'
+  backgroundImage: {
+    flex: 0.5, // Adjust the height to cover half of the screen
   },
-  name: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 5
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)', // Add an overlay to make text readable
+    justifyContent: 'center',
   },
-  contactContainer: {
-    paddingVertical: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: 'red'
+  loginContainer: {
+    paddingHorizontal: 20,
   },
-  sectionContainer: {
-    paddingVertical: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: 'red'
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
   },
-  sectionTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10
-  }
+  forgotPasswordText: {
+    textAlign: 'center',
+    marginTop: 20,
+    color: 'black',
+  },
+  createAccountText: {
+    textAlign: 'center',
+    marginTop: 10,
+    color: 'black',
+  },
 });
 
-export default App;
+export default LoginPage;
---
+++ Skills.js
@@ -0,0 +1,13 @@
+// Skills.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Skills = () => {
+  return (
+    <View>
+      <Text>company a</Text>
+    </View>
+  );
+};
+
+export default Skills;
